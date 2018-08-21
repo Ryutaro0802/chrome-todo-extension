b8c5a13cfe0e41631755ae4d7ab19ef28fa4badf
Ryutaro Kawakami <ne200152g@gmail.com>
2018-08-22 07:41:32 +0900

    submitイベント

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import { App } from './src/App.js';
 
 const app = new App();
+app.mount();
--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 export class App {
-    constructor() {
-        console.log('App init');
-    }
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    formElement.addEventListener("submit", event => {
+      event.preventDefault();
+      console.log(inputElement.value);
+    });
+  }
 }
