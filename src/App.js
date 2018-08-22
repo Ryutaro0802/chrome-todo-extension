4d9fee3d5385a1d805e706df1d16a5d3697b14f2
Ryutaro Kawakami <ne200152g@gmail.com>
2018-08-22 09:50:37 +0900

    リストにTODOを追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { element, escapeSpecialChars } from "./view/html-util.js";
+console.log(escapeSpecialChars`aaa`);
 export class App {
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+    let todoItemCount = 0;
+
     formElement.addEventListener("submit", event => {
       event.preventDefault();
-      console.log(inputElement.value);
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      console.log(todoItemElement);
+      containerElement.appendChild(todoItemElement);
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      inputElement.value = "";
     });
   }
 }
--- src/view/html-util.js
+++ src/view/html-util.js
@@ -8,25 +8,25 @@ export function escapeSpecialChars(str) {
 }
 
 export function htmlToElement(html) {
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.firstElementChild;
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.firstElementChild;
 }
 
 /**
- * HTML文字列からDOM Nodeを作成して返す
+ * HTML 文字列からDOM Nodeを作成して返す
  * @return {HTMLElement}
  */
 export function element(strings, ...values) {
-    const htmlString = strings.reduce((result, string, i) => {
-        const value = values[i - 1];
-        if (typeof value === 'string') {
-            return result + escapeSpecialChars(value) + string;
-        } else {
-            return result + String(value) + string;
-        }
-    });
-    return htmlToElement(htmlString);
+  const htmlString = strings.reduce((result, string, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + string;
+    } else {
+      return result + String(value) + string;
+    }
+  });
+  return htmlToElement(htmlString);
 }
 
 /**
@@ -35,6 +35,8 @@ export function element(strings, ...values) {
  * @param {HTMLElement} containerElement コンテナ要素
  */
 export function render(bodyElement, containerElement) {
-    containerElement.innerHTML = '';
-    containerElement.appendChild(bodyElement);
+  // rootElementの中身を空にする
+  containerElement.innerHTML = "";
+  // rootElementの直下にbodyElementを追加する
+  containerElement.appendChild(bodyElement);
 }
