35fc163c7f5cee8752a86dfd884e705131fb62a3
Ryutaro Kawakami <ne200152g@gmail.com>
2018-08-24 14:04:54 +0900

    doc書いた

--- src/util/store.js
+++ src/util/store.js
@@ -1,7 +1,16 @@
+/**
+ * localStorage に保存
+ * @param {String} storageKey storageKey
+ * @param {String} items storage Items
+ */
 export function setItems(storageKey, items) {
     localStorage.setItem(storageKey, items);
 }
 
+/**
+ * localStorage から取り出す
+ * @param {String} storageKey storageKey
+ */
 export function getItems(storageKey) {
     return localStorage.getItem(storageKey);
 }
